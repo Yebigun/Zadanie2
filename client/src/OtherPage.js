4d0aed75ed25ddf00b27fac30cf522c85cb9c5c3
Yebigun <grzechu982@gmail.com>
2023-01-26 20:44:54 +0100

    starte

---
+++ client/src/OtherPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Multi-fib
+      Grzegorz Zaręba
+      1.5
+      
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
